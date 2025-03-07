8e78bd5fc8bf5bab8fe31188d809faddac95bfbf
Amarjeet Kumar <chauhanamarjeet116@gmail.com>
2025-03-08 03:01:01 +0530

    complete the Login and signup both

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -40,7 +40,7 @@ export default function LoginPage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen py-2">
       <h1 className="text-center text-gray-700 text-2xl mb-4">
-        {loading ? "Processing" : "Login"}
+        {loading ? "Processing" : "Login here"}
       </h1>
 
       <input
