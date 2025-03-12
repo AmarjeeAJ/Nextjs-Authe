be6e4cd3afce3696af497269b23b8ab32f8e535b
Amarjeet Kumar <chauhanamarjeet116@gmail.com>
2025-03-13 01:49:48 +0530

    update the logout components

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,35 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { toast } from "react-hot-toast";
+
 export default function ProfilePage() {
-    return(
-        <div className="flex flex-col items-center justify-center h-screen py-2">
-            <h1 className="text-center text-gray-700 text-2xl mb-4">Profile</h1>
-            <hr/>
-            <p>profile abx</p>
-        </div>
-    )
+  const router = useRouter();
+
+  const logout = () => {
+    try {
+      axios.get("api/users/logout");
+      toast.success("Logout Successful");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen py-2">
+      <h1 className="text-center text-gray-700 text-2xl mb-4">Profile</h1>
+      <hr />
+      <p>profile Page </p>
+      <hr />
+      <button
+        onClick={logout}
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Logout
+      </button>
+    </div>
+  );
 }
