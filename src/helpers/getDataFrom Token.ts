222a6c6602bfa4eb13ff961278beb63e4cf96410
Amarjeet Kumar <chauhanamarjeet116@gmail.com>
2025-03-19 01:16:22 +0530

    update the getdata token and userdetails

--- src/helpers/getDataFrom Token.ts
+++ src/helpers/getDataFrom Token.ts
@@ -1,11 +1,18 @@
 import { NextRequest } from "next/server";
-import  Jwt  from "jsonwebtoken";
+import Jwt from "jsonwebtoken";
 
-export conts getDataFromToken = (request: NextRequest) => {
-    try{
-       const token =  request.cookies.get("token")?.value || '';
-       Jwt.verify(token, process.env.JWT_SECRET_KEY!);
+export const getDataFromToken = (request: NextRequest) => {
+    try {
+        const token = request.cookies.get("token")?.value || '';
+        
+        if (!token) {
+            throw new Error("Token not found");
+        }
+
+        const decodedData = Jwt.verify(token, process.env.JWT_SECRET_KEY!);
+
+        return decodedData; // Return decoded token data
     } catch (error: any) {
-        throw new Error(error.message);
+        throw new Error(`Token verification failed: ${error.message}`);
     }
-}
+};
